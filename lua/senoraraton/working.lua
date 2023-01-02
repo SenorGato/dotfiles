e9f2935f7ff37aa8fc7c0309f62bb38d6e3fe702
SenorGato <senoraraton@gmail.com>
2023-01-01 20:29:29 -0800

    Null-Ls

---
+++ lua/senoraraton/working.lua
@@ -0,0 +1,12 @@
+--local capabilities = vim.lsp.protocol.make_client_capabilities()
+--capabilities.textDocument.foldingRange = {
+    --dynamicRegistration = false,
+    --lineFoldingOnly = true
+--}
+--
+--vim.o.foldcolumn = '1' -- '0' is not bad
+--vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+--vim.o.foldlevelstart = -1
+--vim.o.foldenable = true
+
+--require('ufo').setup()
